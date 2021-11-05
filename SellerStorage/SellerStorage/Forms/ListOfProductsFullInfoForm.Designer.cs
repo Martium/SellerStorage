475f59316f20d75177957c1e22340146578b0c58
Martynas Gedutis <martynasgedutis@gmail.com>
2021-11-05 23:03:39 +0200

    main model for list

--- SellerStorage/SellerStorage/Forms/ListOfProductsFullInfoForm.Designer.cs
+++ SellerStorage/SellerStorage/Forms/ListOfProductsFullInfoForm.Designer.cs
@@ -29,13 +29,37 @@ namespace SellerStorage.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.productsListDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.productsListDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // productsListDataGridView
+            // 
+            this.productsListDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.productsListDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.productsListDataGridView.Location = new System.Drawing.Point(12, 102);
+            this.productsListDataGridView.Name = "productsListDataGridView";
+            this.productsListDataGridView.Size = new System.Drawing.Size(938, 433);
+            this.productsListDataGridView.TabIndex = 1;
+            // 
+            // ListOfProductsFullInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(962, 588);
+            this.Controls.Add(this.productsListDataGridView);
+            this.Name = "ListOfProductsFullInfoForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Produktai";
+            ((System.ComponentModel.ISupportInitialize)(this.productsListDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView productsListDataGridView;
     }
 }
 
