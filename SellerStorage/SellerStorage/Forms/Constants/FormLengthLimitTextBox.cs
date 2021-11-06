8139ed2e876abbf8b268159d4ccc2c1af25a8d72
Martynas Gedutis <martynasgedutis@gmail.com>
2021-11-06 12:08:41 +0200

    CREATE PROduct info table command

--- SellerStorage/SellerStorage/Forms/Constants/FormLengthLimitTextBox.cs
+++ SellerStorage/SellerStorage/Forms/Constants/FormLengthLimitTextBox.cs
@@ -4,14 +4,18 @@
     {
         public static int ProductType => 50;
         public static int ProductDescription => 100;
+
         public static int ProductQuantity => 9;
         public static int ProductQuantityLeft => 9;
+
         public static int ProductOriginalCostPriceCurrency => 12;
         public static int ProductAllQuantityCostPriceAtOriginalCurrency => 12;
+
         public static int ProductQuantityPriceInEuro => 12;
         public static int ProductAllQuantityPriceInEuro => 12;
         public static int ProductExpensesPerQuantityUnit => 12;
         public static int ProductExpectedSellingPrice => 12;
+
         public static int ProductSoldPrice => 12;
         public static int ProductProfit => 12;
     }
--- SellerStorage/SellerStorage/Repository/SqlLiteDataBase/SqlLiteCreateTableCommands.cs
+++ SellerStorage/SellerStorage/Repository/SqlLiteDataBase/SqlLiteCreateTableCommands.cs
@@ -1,10 +1,29 @@
-﻿namespace SellerStorage.Repository.SqlLiteDataBase
+﻿using SellerStorage.Forms.Constants;
+
+namespace SellerStorage.Repository.SqlLiteDataBase
 {
     public class SqlLiteCreateTableCommands
     {
         public static string CreateFullProductInfoTable = 
-            @"
-                
+            $@"
+                CREATE TABLE [FullProductInfoTable]
+                    [ProductId] [Integer] NOT NULL,
+                    [ProductReceiptDate] [Date] NOT NULL,
+
+                    [ProductType] [nvarchar] ({FormLengthLimitTextBox.ProductType}) NULL,
+                    [ProductDescription] [nvarchar] ({FormLengthLimitTextBox.ProductDescription}) NULL,
+
+                    [ProductQuantity] [Integer] ({FormLengthLimitTextBox.ProductQuantity}) NULL,
+                    [ProductQuantityLeft] [Integer] ({FormLengthLimitTextBox.ProductQuantityLeft}) NULL,
+                    
+                    [ProductOriginalCostPriceCurrency] [nvarchar] ({FormLengthLimitTextBox.ProductOriginalCostPriceCurrency}) NULL,
+                    [ProductAllQuantityCostPriceAtOriginalCurrency] [nvarchar] ({FormLengthLimitTextBox.ProductAllQuantityCostPriceAtOriginalCurrency}) NULL,
+                    [ProductQuantityPriceInEuro] [Numeric] NULL,
+                    [ProductAllQuantityPriceInEuro] [Numeric] NULL,
+                    [ProductExpensesPerQuantityUnit] [Numeric] NULL,
+                    [ProductExpectedSellingPrice] [Numeric] NULL,
+                    [ProductSoldPrice] [Numeric] NULL,
+                    [ProductProfit] [Numeric] NULL
             ";
     }
 }
