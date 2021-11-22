64638854c4b7413fef359b06137f718fd97f236e
Martynas Gedutis <martynasgedutis@gmail.com>
2021-11-22 17:06:17 +0200

    number service to parse to number or return zero

---
+++ SellerStorage/UnitTests/NumbersTests/NumberServiceUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SellerStorage.Service;
+
+namespace UnitTests.NumbersTests
+{
+    [TestClass]
+    public class NumberServiceUnitTests
+    {
+        [TestMethod]
+        public void TryParseToNumberOrReturnZero_IsNumber_ReturnsNumber()
+        {
+            var numberService = new NumberService();
+            int expectedNumber = 10;
+
+            int result = numberService.TryParseToNumberOrReturnZero("10");
+
+            Assert.AreEqual(expectedNumber, result);
+        }
+
+        [TestMethod]
+        public void TryParseToNumberOrReturnZero_IsNotNumber_ReturnsZero()
+        {
+            var numberService = new NumberService();
+            int expectedNumber = 0;
+
+            int result = numberService.TryParseToNumberOrReturnZero("Not number");
+
+            Assert.AreEqual(expectedNumber, result);
+        }
+    }
+}
